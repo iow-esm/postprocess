148ee44eca2e711dcd66fac1f1450f1443facb72
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-10-19 17:12:59 +0200

    Added changing name of variables

--- CCLM/process_reference/process_reference.py
+++ CCLM/process_reference/process_reference.py
@@ -15,7 +15,7 @@ reference = config.reference
 sys.path.append('../../auxiliary')
 import create_results_dir
 
-results_dir = create_results_dir(out_dir, from_date, to_date)
+results_dir = create_results_dir.create_results_dir(out_dir, from_date, to_date)
 
 if from_date > 0 and to_date > 0:
     seldate = " -seldate," + str(from_date)[0:4] + "-" + str(from_date)[4:6] + "-" + str(from_date)[6:8] + "," + str(to_date)[0:4] + "-" + str(to_date)[4:6] + "-" + str(to_date)[6:8]
@@ -31,12 +31,29 @@ if reference == "E-OBS":
 for var in variables.keys():	
     for names, numbers in seasons.items():
     
-        os.system("cdo -timmean -selmon," + numbers + seldate +  " -cat \'" + files[var] + "\' " + results_dir + "/" + var + "-" + names + ".nc")
+        output_file = results_dir + "/" + var + "-" + names + ".nc"
+        os.system("cdo -timmean -selmon," + numbers + seldate +  " -cat \'" + files[var] + "\' " + output_file)
         
         os.system("cdo timmin -selmon," + numbers + seldate + " -cat \'" + files[var] + "\' "  + results_dir + "/minfile.nc")
         os.system("cdo timmax -selmon," + numbers + seldate + " -cat \'" + files[var] + "\' "  + results_dir + "/maxfile.nc")
         
         for p in percentiles:
-            os.system("cdo timpctl," + p + " -selmon," + numbers + seldate + " -cat \'" + files[var] + "\' " + results_dir + "/minfile.nc " + results_dir + "/maxfile.nc " + results_dir + "/" + var + "-" + names + "-PCTL_" + p + ".nc")
+            output_file = results_dir + "/" + var + "-" + names + "-PCTL_" + p + ".nc"
+            os.system("cdo timpctl," + p + " -selmon," + numbers + seldate + " -cat \'" + files[var] + "\' " + results_dir + "/minfile.nc " + results_dir + "/maxfile.nc " + output_file)
             
         os.system("rm " + results_dir + "/maxfile.nc " + results_dir + "/minfile.nc ")
+
+if reference == "E-OBS":
+    for var in variables.keys():
+        for names, numbers in seasons.items():
+            output_file = results_dir + "/" + var + "-" + names + ".nc"
+            os.system("cdo chname,longitude,lon " + output_file + " tmp.nc; mv tmp.nc " + output_file)
+            os.system("cdo chname,latitude,lat " + output_file + " tmp.nc; mv tmp.nc " + output_file)
+            os.system("cdo chname," + variables[var].split("_")[0] + "," + var + " " + output_file + " tmp.nc; mv tmp.nc " + output_file)
+            
+            for p in percentiles:
+                output_file = results_dir + "/" + var + "-" + names + "-PCTL_" + p + ".nc"
+                os.system("cdo chname,longitude,lon " + output_file + " tmp.nc; mv tmp.nc " + output_file)
+                os.system("cdo chname,latitude,lat " + output_file + " tmp.nc; mv tmp.nc " + output_file)
+                os.system("cdo chname," + variables[var].split("_")[0] + "," + var + " " + output_file + " tmp.nc; mv tmp.nc " + output_file)
+            
