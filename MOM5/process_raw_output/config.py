9bcd942c9044dd5b6c123e2882cab06e7569ea8d
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-10-20 17:10:49 +0200

    Started with MOM's raw output processing

---
+++ MOM5/process_raw_output/config.py
@@ -0,0 +1,2 @@
+files_to_process = ["ocean_day2d", "ocean_day3d"]
+path_to_mppn = "../../../components/MOM5/src/postprocessing/mppnccombine" # relative to this directory 
---
+++ MOM5/process_raw_output/process_raw_output.py
@@ -0,0 +1,58 @@
+import glob
+import os
+import sys
+
+out_dir = str(sys.argv[1])
+from_date = int(sys.argv[2])
+to_date = int(sys.argv[3])
+
+from config import files_to_process, path_to_mppn
+
+sys.path.append('../../auxiliary')
+import get_all_dirs_from_to
+
+dirs = get_all_dirs_from_to.get_all_dirs_from_to(out_dir, from_date, to_date)
+
+for dir in dirs:
+    
+    if glob.glob(dir + "/out_raw") == []:
+        continue
+    
+    print(dir)
+    
+    for f in files_to_process:
+        
+        year = dir.split("/")[-1][0:4]
+        month = dir.split("/")[-1][4:6]
+        
+        files_pattern = f + "_" + year + "_" + month + ".nc.????"
+        
+        if glob.glob(dir + "/out_raw/" + files_pattern) == []:
+            continue
+        
+        print(f)
+        
+        pwd = os.getcwd()
+    
+        command = "cd " + dir + "/out_raw/; "
+        command += pwd + "/" + path_to_mppn + "/mppnccombine -n4 -d 3 -m -k 11 -r " + files_pattern
+        os.system(command)
+        
+        output_file = f + "_" + year + "_" + month + ".nc"
+        
+        if glob.glob(dir + "/out_raw/" + output_file) == []:
+            print("Error: " + output_file + " could not be found.")
+            continue
+        
+        os.system("mv " + dir + "/out_raw/" + output_file + " " + dir + "/" + output_file)
+        os.system("rm " + dir + "/out_raw/" + files_pattern)
+        
+        command = "cd " + dir + "/; "
+        command += "for var in `cdo -showname " +  output_file +" 2> /dev/null`; do "
+        command += "cdo -selname,$var " + output_file + " $var.nc 2> /dev/null; done; "
+        command += "rm " + output_file
+        os.system(command)
+        
+        
+
+
