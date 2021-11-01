81c7c0a6b780dc232277511fa0d7fa701cce7058
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-11-01 15:52:32 +0100

    Comment in seasons and percentiles

--- MOM5/process_reference/config.py
+++ MOM5/process_reference/config.py
@@ -1,11 +1,11 @@
 seasons = {
     "MAM" : "3,4,5",
-#    "JJA" : "6,7,8",
-#    "SON" : "9,10,11",
-#    "DJF" : "12,1,2"
+    "JJA" : "6,7,8",
+    "SON" : "9,10,11",
+    "DJF" : "12,1,2"
 }
 
-percentiles = []#["95", "5"]#, "25"]
+percentiles = ["95", "5"]#, "25"]
 
 reference = "Copernicus"
 
@@ -23,6 +23,7 @@ if reference == "Copernicus":
     # fraction of ice
     "FI" : { "file-pattern" : "*-Baltic-ESACCI-L4_GHRSST-SSTdepth-OSTIA-GLOB_CDR2.0-v02.0-fv01.0.nc",
              "name" : "sea_ice_fraction",
+             "remapping-file" : "../create_remapping_files/results/_scratch_usr_mvkkarst_IOW_ESM_output_RUNXX_MOM5_Baltic-19810901_20091130/grid_FI.txt",
              "seasons" : seasons,
              "percentiles" : percentiles
            }              
