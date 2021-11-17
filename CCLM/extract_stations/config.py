61afbb6a6c896fc43fcf72632c0225850c62b19d
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-11-17 10:03:59 +0100

    Added some stations around the Baltic Sea

--- CCLM/extract_stations/config.py
+++ CCLM/extract_stations/config.py
@@ -1,7 +1,14 @@
 dependencies = ["process_reference", "process_raw_output"]
 
 stations = { 
-    "ROSTOCK-WARNEMUNDE" : {"lat" : "54.18", "lon" : "12.08"}                                
+    "ROSTOCK-WARNEMUNDE" : {"lat" : "54.18", "lon" : "12.08"},
+    "HELSINKI-KAISANIEMI" : {"lat" : "60.18", "lon" : "24.95"},
+    "STOCKHOLM" : {"lat" : "59.35", "lon" : "18.05"},
+    "TALLINN" : {"lat" : "59:23:53", "lon" : "24:36:10"},
+    "VISBY" : {"lat" : "57:40:00", "lon" : "18:19:59"},
+    "SUNDSVALL" : {"lat" : "62:24:36", "lon" : "17:16:12"},
+    "LULEA" : {"lat" : "65:37:12", "lon" : "22:07:48"},
+    "VAASA-PALOSAARI" : {"lat" : "63:06:00", "lon" : "21:36:00"},
 }
 
 operators = ["-monmean", "-seasmean", "-ymonmean", "-yseasmean"]
