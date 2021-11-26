d1dc13c402d882066d5fdaec54c3fd9cf868c343
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-11-26 13:56:31 +0100

    Removed redundant dependencies and module loads

--- CCLM/create_remapping_files/postprocess.sh
+++ CCLM/create_remapping_files/postprocess.sh
@@ -2,9 +2,4 @@ out_dir=$1
 from_date=${2:--1}
 to_date=${3:--1}
 
-dependencies=()
-
-module load cdo
-module load anaconda3
-
 python3 ../../auxiliary/create_remapping_files.py "${out_dir}" ${from_date} ${to_date} "$PWD"
--- MOM5/seasonal_mean/postprocess.sh
+++ MOM5/seasonal_mean/postprocess.sh
@@ -2,9 +2,4 @@ out_dir=$1
 from_date=${2:--1}
 to_date=${3:--1}
 
-dependencies=("process_raw_output" "extract_levels")
-
-module load cdo
-module load anaconda3
-
 python3 ../../auxiliary/seasonal_mean.py "${out_dir}" ${from_date} ${to_date} "$PWD"
