02f455cc7524d74e6b92c6d771967af1b032b103
Sven Karsten <sven.karsten@io-warnemuende.de>
2022-01-28 14:07:10 +0100

    Shortened name of results directory.
    Since output directory is always relative to output/ the name of
    the results directory can be shortened.

--- auxiliary/create_results_dir.py
+++ auxiliary/create_results_dir.py
@@ -3,7 +3,8 @@ import os
 def create_results_dir(out_dir, from_date, to_date):
 
     # relate results uniquely to path to data (replace slashes by underscores)
-    results_dir = "results/" + out_dir.replace("/","_")
+    output_folder = out_dir.split("/output/")[-1]
+    results_dir = "results/" + output_folder.replace("/","_")
     
     # if limits are given, add them to the name 
     if from_date > 0 and to_date > 0:
