6681273c803d608bbb23952c5030d821f4a1f927
Sven Karsten <sven.karsten@io-warnemuende.de>
2022-08-10 14:22:45 +0200

    Removed unnecessary loadings of modules

--- MOM5/plot/postprocess.sh
+++ MOM5/plot/postprocess.sh
@@ -2,9 +2,4 @@ out_dir=$1
 from_date=${2:--1}
 to_date=${3:--1}
 
-dependencies=("seasonal_mean" "seasonal_percentile" "process_reference")
-
-module load cdo
-module load anaconda3
-
 python3 ../../auxiliary/plot_on_map.py "${out_dir}" ${from_date} ${to_date} "$PWD"
--- MOM5/prepare_for_validator/postprocess.sh
+++ MOM5/prepare_for_validator/postprocess.sh
@@ -2,14 +2,6 @@ out_dir=$1
 from_date=${2:--1}
 to_date=${3:--1}
 
-# depends only on processed raw output
-dependencies=("process_raw_output")
-
-# load necessary modules
-module load cdo
-module load anaconda3
-module load git
-
 # get the most current validator_peparator from IOW's git server
 if [ -d "./validator_peparator/.git" ]; then
 	cd "./validator_peparator"
