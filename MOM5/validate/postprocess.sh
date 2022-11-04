382af0da2cd760221bd6b21ddd95e52e54e2a164
Sven Karsten <sven.karsten@io-warnemuende.de>
2022-11-04 06:39:32 +0100

    Added validate and analyze tasks

---
+++ MOM5/validate/postprocess.sh
@@ -0,0 +1,13 @@
+out_dir=$1
+from_date=${2:--1}
+to_date=${3:--1}
+
+#----------------------------------------------------
+
+cd ../analyze
+./postprocess.sh ${out_dir} ${from_date} ${to_date}
+
+#----------------------------------------------------
+
+cd ../create_validation_report
+./postprocess.sh ${out_dir} ${from_date} ${to_date} &
