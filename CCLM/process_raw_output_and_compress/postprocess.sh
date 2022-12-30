cda68d786bed9ded0b481aac7a0d5b37bf3f7388
Sven Karsten <sven.karsten@io-warnemuende.de>
2022-12-30 12:55:52 +0100

    Added new process_raw_output_and_compress task
    Monthly average of output is calculated
    and the original data is tar'ed and compressed.
    Uncompressed files are removed.

---
+++ CCLM/process_raw_output_and_compress/postprocess.sh
@@ -0,0 +1,16 @@
+out_dir=$1
+from=$2
+to=$3
+
+cd ../process_raw_output/
+./postprocess.sh ${out_dir} $from $to
+cd -
+
+source ./config.sh
+source ../../auxiliary/process_raw_output_and_compress.sh ${out_dir} $from $to
+
+
+
+
+
+
