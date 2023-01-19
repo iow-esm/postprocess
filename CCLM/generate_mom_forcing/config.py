5d5a82610913adf96dab2d3ba3d8577aee03f6d4
Sven Karsten <sven.karsten@io-warnemuende.de>
2023-01-19 07:52:06 +0100

    Changed remapping to 3nm setup

--- CCLM/generate_mom_forcing/config.py
+++ CCLM/generate_mom_forcing/config.py
@@ -1,8 +1,10 @@
 # this depends on a processed raw output
 dependencies = ["process_raw_output"]
 
+max_jobs = 4
+
 # Important: the following CCLM variables are used 
-# "T_2M", "U_10M", "V_10M", "PMSL", "CLCT", "QV_2M", "TOT_PREC", "ASOB_S", "ALWD_S"
+# "T_2M", "U_10M", "V_10M", "PMSL", "CLCT", "QV_2M", "TOT_PREC", "ASWD_S", "ALWD_S"
 # to generate the forcing, thus they should have been printed out with a reasonable time resolution!
 
 # you can either specify a path to a remapping file (compatible to the remapbil operator of cdo)
@@ -11,10 +13,11 @@ dependencies = ["process_raw_output"]
 # or you can specify the remapping directly here (also compatible to the remapbil operator of cdo)
 remapping = """
 gridtype  = lonlat
-xsize     = 91
-ysize     = 102
-xfirst    = 8.12
-xinc      = 0.24
-yfirst    = 53.64
-yinc      = 0.119999999999999
+xsize     = 224
+ysize     = 242
+xfirst    = 8.28
+xinc      = 0.1
+yfirst    = 53.85
+yinc      = 0.05
 """
+
